fd4f98c001a637b82b259a2c2742f60f46808efa
ficopang <pangestufico@gmail.com>
2023-05-20 13:12:30 +0700

    fix CRUD product

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -16,17 +16,18 @@
             @method('PUT')
             <div class="mb-3">
                 <label for="product-name" class="form-label">Product Name</label>
-                <input type="text" class="form-control" id="product-name" name="product-name" value="{{ $product->name }}"
-                    required>
+                <input type="text" class="form-control" id="product-name" name="product-name"
+                    value="{{ $product->name }}" required>
             </div>
             <div class="mb-3">
                 <label for="category-select" class="form-label">Select a Category</label>
                 <select class="form-select" id="category-select" name="category">
                     <option selected disabled>Select a category</option>
-                    <option value="1" {{ $product->category_id == 1 ? 'selected' : '' }}>Electronics</option>
-                    <option value="2" {{ $product->category_id == 2 ? 'selected' : '' }}>Clothing</option>
-                    <option value="3" {{ $product->category_id == 3 ? 'selected' : '' }}>Home</option>
-                    <option value="4" {{ $product->category_id == 4 ? 'selected' : '' }}>Beauty</option>
+                    @foreach ($categories as $category)
+                        <option
+                            value="{{ $category['id'] }}"{{ $category['id'] == $product->category_id ? 'selected' : '' }}>
+                            {{ $category['name'] }}</option>
+                    @endforeach
                 </select>
             </div>
             <div class="mb-3">
@@ -35,27 +36,28 @@
             </div>
             <div class="mb-3">
                 <label for="starting-price" class="form-label">Starting Price</label>
-                <input type="number" class="form-control" id="starting-price" name="starting-price" value="{{ $product->starting_price }}"
-                    required step="1000">
+                <input type="number" class="form-control" id="starting-price" name="starting-price"
+                    value="{{ $product->starting_price }}" required step="1000">
             </div>
             <div class="mb-3">
                 <label for="min-bid-increment" class="form-label">Minimum Bid Increment</label>
-                <input type="number" class="form-control" id="min-bid-increment" name="min-bid-increment" value="{{ $product->min_bid_increment }}"
-                    required step="1000">
+                <input type="number" class="form-control" id="min-bid-increment" name="min-bid-increment"
+                    value="{{ $product->min_bid_increment }}" required step="1000">
             </div>
             <div class="mb-3">
                 <label for="product-image" class="form-label">Product Image</label>
-                <input type="file" class="form-control" id="product-image" name="product-image" accept="image/*" required>
+                <input type="file" class="form-control" id="product-image" name="product-image" accept="image/*"
+                    required>
             </div>
             <div class="mb-3">
                 <label for="start-time" class="form-label">Start Time</label>
-                <input type="datetime-local" class="form-control" id="start-time" name="start-time" value="{{ $product->start_time }}"
-                    required>
+                <input type="datetime-local" class="form-control" id="start-time" name="start-time"
+                    value="{{ $product->start_time }}" required>
             </div>
             <div class="mb-3">
                 <label for="end-time" class="form-label">End Time</label>
-                <input type="datetime-local" class="form-control" id="end-time" name="end-time" value="{{ $product->end_time }}"
-                    required>
+                <input type="datetime-local" class="form-control" id="end-time" name="end-time"
+                    value="{{ $product->end_time }}" required>
             </div>
 
             <div style="margin-top: 40px;">
